6c38f2bf636c85b492232e7dba011ae258cdf30f
沖藤　壱太 <2524046@i-seifu.jp>
2026-09-25 12:06:40 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-echo "Amazon Linux Webサービス レベル1 稼働中";
+setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("DB接続エラー: " . $e->getMessage());
+}
+
+$message = "";
+if (\(_SERVER['REQUEST_METHOD'] === 'POST' && !empty(\)_POST['name']) && !empty($_POST['song'])) {
+    \(stmt =\)pdo->prepare("INSERT INTO participants (name, song) VALUES (:name, :song)");
+    \(stmt->execute([':name' =>\)_POST['name'], ':song' => $_POST['song']]);
+    $message = "参加申し込みが正常に完了しました！";
+}
+
+\(stmt =\)pdo->query("SELECT * FROM participants ORDER BY id DESC");
+\(participants =\)stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
+
+
+    
+    カラオケ大会 参加申し込みシステム
